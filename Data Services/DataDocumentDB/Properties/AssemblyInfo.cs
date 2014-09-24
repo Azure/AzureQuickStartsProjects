084ba2def6dcbb01b058d2e888eb9bd7775ca87e
Ryan CrawCour <ryancrawcour@outlook.com>
2014-09-24 16:46:31 -0700

    First drop
    Initial drop of DocumentDB stuff.

--- Data Services/DataDocumentDB/Properties/AssemblyInfo.cs
+++ Data Services/DataDocumentDB/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DeployManageVirtualNetwork")]
+[assembly: AssemblyTitle("DataDocumentDB")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DeployManageVirtualNetwork")]
+[assembly: AssemblyProduct("DataDocumentDB")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("904f581d-b0f5-495e-be5c-24e9382d0644")]
+[assembly: Guid("90433c78-0480-46e8-b4f2-5842005d3285")]
 
 // Version information for an assembly consists of the following four values:
 //
