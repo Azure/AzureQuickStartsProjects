3dda659406d51390d69f99795136ef51b2095dc7
Cory Fowler <cfowler@microsoft.com>
2014-10-07 20:34:34 -0700

    created new csproj files for WebJobsSDK samples as they were built with update 4

--- Compute/ComputeWebJobsSDKSvcBusQ/Properties/AssemblyInfo.cs
+++ Compute/ComputeWebJobsSDKSvcBusQ/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ComputeWebJobsSDKServiceBus")]
+[assembly: AssemblyTitle("ComputeWebJobsSDKSvcBusQ")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ComputeWebJobsSDKServiceBus")]
+[assembly: AssemblyProduct("ComputeWebJobsSDKSvcBusQ")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6773443e-09b0-4dbe-91b1-685250c3b027")]
+[assembly: Guid("6045f1c0-1b65-4aa6-8f03-92dfdb600c2d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
